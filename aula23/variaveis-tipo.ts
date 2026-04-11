0db98ece3e0faa3ca57e6d615e483b5082c10dfa
evertonulisystem <evertonulisystem@gmail.com>
2026-04-11 15:06:42 -0300

    feat: (aula23 ) sobre variaveis, tipos e nomenclaturas de pasta e arquivos

---
+++ aula23/variaveis-tipo.ts
@@ -0,0 +1,11 @@
+let statusPedido = 'Pendente'
+let primeiroNome: string = 'Maria'
+let ativo = false
+let ativado: boolean = true
+let precoUnitario: number = 154.55
+let precoTotal = 300
+
+const BASE_URL = 'https://northwind-test-platform.vercel.app/api-docs'
+
+console.log(ativo)
+console.log('O nome do usuário é:', primeiroNome)
