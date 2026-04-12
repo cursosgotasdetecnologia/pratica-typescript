fcc0b50b7f62283d42cfda47fc37728bc3844aea
evertonulisystem <evertonulisystem@gmail.com>
2026-04-12 11:12:17 -0300

    feat: (aula27) arrow-functions.ts

---
+++ aula27/arrow-functions.ts
@@ -0,0 +1,18 @@
+// function calcularTotal(preco: number): number {
+//     return preco * 10; 
+// }
+
+// const valorTotal = calcularTotal(100);
+
+// console.log('Preço total:', valorTotal);
+
+
+/////////////////////////
+
+const calcularTotal = (preco: number): number => {
+    return preco * 10; 
+}
+
+console.log('Resultado Moderno:', calcularTotal(100));
+
+//test('login', () => {  })
